094386ea32e2958b841b47ca230dc484a4e9ebb4
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2018-01-18 19:46:26 +0530

    third commit

--- server/server.js
+++ server/server.js
@@ -16,20 +16,37 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   socket.emit('newMessage', {
-    from: 'John',
-    text: 'See you then',
-    createdAt: 123123
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
   });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
-
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
 });
+
+
 
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
